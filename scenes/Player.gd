25207d0eda34b74817f65675f568a5e4602a84ba
KenKomKom <kenichikomala@gmail.com>
2025-02-23 15:45:46 +0700

    update to 4.3

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,13 +1,10 @@
-extends KinematicBody2D
+extends CharacterBody2D
 
 const UP = Vector2(0, -1)
 
-export var speed: int = 400
-export var GRAVITY: int = 1200
-export var jump_speed: int = -400
-
-var velocity: Vector2 = Vector2()
-
+@export var speed: int = 400
+@export var GRAVITY: int = 1200
+@export var jump_speed: int = -400
 
 func get_input():
 	velocity.x = 0
@@ -18,12 +15,13 @@ func get_input():
 	if Input.is_action_pressed("left"):
 		velocity.x -= speed
 
-
-func _physics_process(_delta):
+func _physics_process(delta):
 	velocity.y += delta * GRAVITY
 	get_input()
-	velocity = move_and_slide(velocity, UP)
-
+	set_velocity(velocity)
+	set_up_direction(UP)
+	move_and_slide()
+	velocity = velocity
 
 func _process(_delta):
 	if velocity.y != 0:
@@ -31,8 +29,8 @@ func _process(_delta):
 	elif velocity.x != 0:
 		$Animator.play("Walk")
 		if velocity.x > 0:
-			$Sprite.flip_h = false
+			$Sprite2D.flip_h = false
 		else:
-			$Sprite.flip_h = true
+			$Sprite2D.flip_h = true
 	else:
 		$Animator.play("Idle")
